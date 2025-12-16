4a0d4f7ff9e509e5a5fea43536d622f479c9cd7d
azim khan <azimkhan50607@gmail.com>
2025-12-16 18:29:04 +0530

    refactor: load environment variables for backend URL in Vite configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+export default defineConfig(({ mode }) => {
+  // env load karo
+  const env = loadEnv(mode, process.cwd(), '')
 
-// https://vite.dev/config/
-export default defineConfig({                                                                                                                               
-  plugins: [react(),tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_BACKEND_BASE_URL,
-        changeOrigin: true,
-        secure: false,
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-
+  }
 })

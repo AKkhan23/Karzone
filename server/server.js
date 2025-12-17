a6aef6e8b2235b6874f516bcbb822604cf9f1d6f
azim khan <azimkhan50607@gmail.com>
2025-12-17 16:03:01 +0530

    fix: restore CORS middleware configuration in server setup

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const dotenv = require("dotenv");
 const connectDB = require("./config/dbConfig");
 const colors = require("colors");
-// const cors = require("cors");
+const cors = require("cors");
 
 dotenv.config();
 connectDB();
@@ -13,14 +13,20 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("➡️ Request:", req.method, req.originalUrl);
+  console.log("🌍 Origin:", req.headers.origin);
+  next();
+});
 
-// app.use(
-//   cors({
-//     origin: "https://karzone-4rrno0wwk-azim-khans-projects-e5bdc5a4.vercel.app",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
+
+app.use(
+  cors({
+    origin: "https://karzone-z9pw.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to car rental Api" });
